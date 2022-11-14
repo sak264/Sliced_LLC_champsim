79ac6939c3d7b26dfddbe585c8da0157bb862fc8
sak264 <saksham.mehra@outlook.com>
2022-11-14 12:58:42 +0530

    Added code for sliced LLC with 2 cores and  modulo hash function

---
+++ Champsim/src/l2c.d
@@ -0,0 +1,53 @@
+src/l2c.o: src/l2c.cc src/l2c.h inc/cache.h inc/champsim.h \
+ inc/champsim_constants.h inc/util.h inc/delay_queue.hpp \
+ inc/circular_buffer.hpp inc/memory_class.h inc/block.h inc/instruction.h \
+ inc/trace_instruction.h inc/ooo_cpu.h inc/operable.h \
+ inc/ooo_cpu_modules.inc inc/cache_modules.inc inc/champsim.h \
+ inc/delay_queue.hpp inc/memory_class.h inc/ooo_cpu.h inc/operable.h \
+ inc/champsim_constants.h inc/util.h inc/vmem.h
+
+src/l2c.h:
+
+inc/cache.h:
+
+inc/champsim.h:
+
+inc/champsim_constants.h:
+
+inc/util.h:
+
+inc/delay_queue.hpp:
+
+inc/circular_buffer.hpp:
+
+inc/memory_class.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/trace_instruction.h:
+
+inc/ooo_cpu.h:
+
+inc/operable.h:
+
+inc/ooo_cpu_modules.inc:
+
+inc/cache_modules.inc:
+
+inc/champsim.h:
+
+inc/delay_queue.hpp:
+
+inc/memory_class.h:
+
+inc/ooo_cpu.h:
+
+inc/operable.h:
+
+inc/champsim_constants.h:
+
+inc/util.h:
+
+inc/vmem.h:
---
+++ Champsim/src/l2c.h
@@ -0,0 +1,132 @@
+#ifndef L2C_H
+#define L2C_H
+
+#include <functional>
+#include <list>
+#include <string>
+#include <vector>
+
+#include "cache.h"
+#include "champsim.h"
+#include "delay_queue.hpp"
+#include "memory_class.h"
+#include "ooo_cpu.h"
+#include "operable.h"
+
+// virtual address space prefetching
+#define VA_PREFETCH_TRANSLATION_LATENCY 2
+
+extern std::array<O3_CPU *, NUM_CPUS> ooo_cpu;
+
+class L2C : public CACHE {
+public:
+  // uint32_t cpu;
+  // const std::string NAME;
+  // const uint32_t NUM_SET, NUM_WAY, WQ_SIZE, RQ_SIZE, PQ_SIZE, MSHR_SIZE;
+  // const uint32_t HIT_LATENCY, FILL_LATENCY, OFFSET_BITS;
+  // std::vector<BLOCK> block{NUM_SET * NUM_WAY};
+  // const uint32_t MAX_READ, MAX_WRITE;
+  // uint32_t reads_available_this_cycle, writes_available_this_cycle;
+  // const bool prefetch_as_load;
+  // const bool match_offset_bits;
+  // const bool virtual_prefetch;
+  // bool ever_seen_data = false;
+  // const unsigned pref_activate_mask =
+  //     (1 << static_cast<int>(LOAD)) | (1 << static_cast<int>(PREFETCH));
+
+  // // prefetch stats
+  // uint64_t pf_requested = 0, pf_issued = 0, pf_useful = 0, pf_useless = 0,
+  //          pf_fill = 0;
+
+  // // queues
+  // champsim::delay_queue<PACKET> RQ{RQ_SIZE, HIT_LATENCY}, // read queue
+  //     PQ{PQ_SIZE, HIT_LATENCY},                           // prefetch queue
+  //     VAPQ{PQ_SIZE,
+  //          VA_PREFETCH_TRANSLATION_LATENCY}, // virtual address prefetch queue
+  //     WQ{WQ_SIZE, HIT_LATENCY};              // write queue
+
+  // std::list<PACKET> MSHR; // MSHR
+
+  // uint64_t sim_access[NUM_CPUS][NUM_TYPES] = {},
+  //          sim_hit[NUM_CPUS][NUM_TYPES] = {},
+  //          sim_miss[NUM_CPUS][NUM_TYPES] = {},
+  //          roi_access[NUM_CPUS][NUM_TYPES] = {},
+  //          roi_hit[NUM_CPUS][NUM_TYPES] = {},
+  //          roi_miss[NUM_CPUS][NUM_TYPES] = {};
+
+  // uint64_t RQ_ACCESS = 0, RQ_MERGED = 0, RQ_FULL = 0, RQ_TO_CACHE = 0,
+  //          PQ_ACCESS = 0, PQ_MERGED = 0, PQ_FULL = 0, PQ_TO_CACHE = 0,
+  //          WQ_ACCESS = 0, WQ_MERGED = 0, WQ_FULL = 0, WQ_FORWARD = 0,
+  //          WQ_TO_CACHE = 0;
+
+  // uint64_t total_miss_latency = 0;
+
+  const uint32_t DECODER_LOGIC_LATENCY = 3,DECODER_REDIRECTION_LATENCY = 1;
+  // functions
+  int add_rq(PACKET *packet) override;
+  int add_wq(PACKET *packet) override;
+  int add_pq(PACKET *packet) override;
+
+  void return_data(PACKET *packet) override;
+  void operate() override;
+  void operate_writes();
+  void operate_reads();
+
+  uint32_t get_occupancy(uint8_t queue_type, uint64_t address) override;
+  uint32_t get_size(uint8_t queue_type, uint64_t address) override;
+
+  uint32_t get_set(uint64_t address);
+  uint32_t get_way(uint64_t address, uint32_t set);
+
+  int invalidate_entry(uint64_t inval_addr);
+  int prefetch_line(uint64_t pf_addr, bool fill_this_level,
+                    uint32_t prefetch_metadata);
+  int prefetch_line(uint64_t ip, uint64_t base_addr, uint64_t pf_addr,
+                    bool fill_this_level,
+                    uint32_t prefetch_metadata); // deprecated
+
+  void add_mshr(PACKET *packet);
+  void va_translate_prefetches();
+
+  void handle_fill();
+  void handle_writeback();
+  void handle_read();
+  void handle_prefetch();
+
+  void readlike_hit(std::size_t set, std::size_t way, PACKET &handle_pkt);
+  bool readlike_miss(PACKET &handle_pkt);
+  // bool filllike_miss(std::size_t set, std::size_t way, PACKET &handle_pkt);
+
+  bool should_activate_prefetcher(int type);
+
+  void print_deadlock() override;
+
+// #include "cache_modules.inc"
+
+//   const repl_t repl_type;
+//   const pref_t pref_type;
+
+  // NOTE - NEW DECLARATIONS
+  vector<MemoryRequestConsumer *> all_lower_levels;
+
+  uint32_t get_slice(uint32_t cpu, uint64_t address);
+  bool filllike_miss(std::size_t set, std::size_t way, std::size_t slice,
+                     PACKET &handle_pkt);
+
+  // constructor
+  L2C(std::string v1, double freq_scale, unsigned fill_level, uint32_t v2,
+      int v3, uint32_t v5, uint32_t v6, uint32_t v7, uint32_t v8,
+      uint32_t hit_lat, uint32_t fill_lat, uint32_t max_read,
+      uint32_t max_write, std::size_t offset_bits, bool pref_load,
+      bool wq_full_addr, bool va_pref, unsigned pref_act_mask,
+      vector<MemoryRequestConsumer *> lls,
+      pref_t pref, repl_t repl)
+      : CACHE(v1, freq_scale, fill_level, v2, v3, v5, v6, v7, v8, hit_lat,
+              fill_lat, max_read, max_write, offset_bits, pref_load,
+              wq_full_addr, va_pref, pref_act_mask, lls[0], pref, repl) {
+    //assert(lls.size() == NUM_CPUS);
+    all_lower_levels = std::move(lls);
+  }
+};
+
+#endif
